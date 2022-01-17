20a986d521d3f158392ff1fe3a8d1056959bd1cb
alexvlamakis <alexvlamakis@gmail.com>
2022-01-16 18:32:31 -0700

    code clean up

--- server.js
+++ server.js
@@ -1,28 +1,88 @@
-// Dependencies
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const database = require("./db/db")
 
-// Point Server to the route files
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+var app = express();
+var PORT = process.env.PORT || 3001;
 
-// Create an express server
-const app = express();
+// Link to assets in public folder
+app.use(express.static('public'));
 
-// Set PORT
-const PORT = process.env.PORT || 3001;
 
-// Parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-
-// Parse incoming JSON data
 app.use(express.json());
 
 
-app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
-// Listener
-app.listen(PORT, () => {
-    console.log(`API server is ready on port ${PORT}!`);
+//Notes html and it's "url"
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
+
+app.route("/api/notes")
+    //Obtain the new and current notes
+    .get(function (req, res) {
+        res.json(database);
+    })
+
+    // Add a new note to the json db file.
+    .post(function (req, res) {
+        let jsonFilePath = path.join(__dirname, "/db/db.json");
+        let newNote = req.body;
+
+        // This allows the test note to be the original note.
+        let highestId = 99;
+        // This loops through the array and finds the highest ID.
+        for (let i = 0; i < database.length; i++) {
+            let individualNote = database[i];
+
+            if (individualNote.id > highestId) {
+                highestId = individualNote.id;
+            }
+        }
+        //This assigns an ID to the newNote. 
+        newNote.id = highestId + 1;
+        //Push it to db.json.
+        database.push(newNote)
+
+        fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
+
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Your note was saved!");
+        });
+        // Gives back the response, which is the user's new note. 
+        res.json(newNote);
+    });
+
+app.delete("/api/notes/:id", function (req, res) {
+    let jsonFilePath = path.join(__dirname, "/db/db.json");
+    //request to delete note by id.
+    for (let i = 0; i < database.length; i++) {
+
+        if (database[i].id == req.params.id) {
+            //splice shows as i to delete 1
+            database.splice(i, 1);
+            break;
+        }
+    }
+    // Write the db.json file again.
+    fs.writeFileSync(jsonFilePath, JSON.stringify(database), function (err) {
+
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("Your note was deleted!");
+        }
+    });
+    res.json(database);
+});
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });
